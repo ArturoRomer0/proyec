2e7ee5618c269725a8a997e311390428174d6ab2
booker <pepe50a@gmail.com>
2024-05-05 22:16:23 -0600

    modificiacion1

--- frontend/src/views/Footer.vue
+++ frontend/src/views/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="">
+  <footer class=" ">
    
   </footer>
   
---
+++ frontend/src/views/Horario.vue
@@ -0,0 +1,27 @@
+<template>
+<templateView>
+    <template>
+        <div>
+            <h2>Citas</h2>
+            
+        </div>
+
+    </template>
+</templateView>
+</template>
+
+<script setup>
+
+import templateView from '@/views/Template.vue';
+
+export default {
+    name: 'HorarioView',
+    components: {
+      templateView,
+    }
+  }
+
+</script>
+
+<style scoped>
+</style>
